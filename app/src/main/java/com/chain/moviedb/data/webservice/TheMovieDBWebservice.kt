ce4b62312744e96a8a038569c8a0f9e92a68a1aa
harkairt <harkai.robert.tamas@gmail.com>
2019-06-16 13:53:00 +0200

    Put out apiKey into Constants

--- app/src/main/java/com/chain/moviedb/data/webservice/TheMovieDBWebservice.kt
+++ app/src/main/java/com/chain/moviedb/data/webservice/TheMovieDBWebservice.kt
@@ -2,6 +2,7 @@ package com.chain.moviedb.data.webservice
 
 import com.chain.moviedb.data.MovieDBSearchResponse
 import com.chain.moviedb.domain.entities.Movie
+import com.chain.moviedb.util.apiKey
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -9,11 +10,11 @@ import retrofit2.http.Query
 
 interface TheMovieDBWebservice {
 
-    @GET("search/movie?api_key=43a7ea280d085bd0376e108680615c7f")
+    @GET("search/movie?api_key=$apiKey")
     fun searchMovies(@Query("query") query: String) : Single<MovieDBSearchResponse>
 
 
-    @GET("movie/{movieId}?api_key=43a7ea280d085bd0376e108680615c7f")
+    @GET("movie/{movieId}?api_key=$apiKey")
     fun getMovieDetails(@Path("movieId") movieId: Int) : Single<Movie>
 
 }
--- app/src/main/java/com/chain/moviedb/util/Constants.kt
+++ app/src/main/java/com/chain/moviedb/util/Constants.kt
@@ -4,3 +4,5 @@ const val posterBasePath = "https://image.tmdb.org/t/p/w500"
 
 const val movieIdKey = "MOVIE_ID"
 
+const val apiKey = "43a7ea280d085bd0376e108680615c7f"
+
